ab072274b68cd7b2ae7ca755b073351749cbc345
Quillan Ogle <quillan.ogle@ruralsource.com>
2019-11-05 14:39:28 -0500

    landing page, move the bracket view to button, added material-ui

---
+++ pages/Bracket.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import BracketView from "../components/BracketView";
+
+const Bracket = () => {
+    return (
+        <div style={{ height: "100vh", width: "100vw" }}>
+            <BracketView />
+        </div>
+    );
+};
+
+export default Bracket;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import Head from 'next/head';
-import Nav from '../components/nav';
+import React from "react";
+import Head from "next/head";
+import Nav from "../components/nav";
 
-import dynamic from 'next/dynamic';
-
-const BracketView = dynamic(() => import('../components/BracketView'), { ssr: false });
+import dynamic from "next/dynamic";
+import Link from "next/link";
+import { Button } from "@material-ui/core";
 
 const Home = () => {
-	return (
-		<div style={{ height: '100vh', width: '100vw' }}>
-			<BracketView />
-		</div>
-	);
+    return (
+        <div>
+            <Link href="/Bracket" passHref>
+                <Button component="a">Build a Bracket</Button>
+            </Link>{" "}
+        </div>
+    );
 };
 
 export default Home;
