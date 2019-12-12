8f6c7912d22c8b8946bffb1ff814d1b974c48932
Quillan Ogle <quillan.ogle@ruralsource.com>
2019-12-12 09:56:03 -0500

    SemiFinal refactor, select8 refactor

--- pages/select4.tsx
+++ pages/select4.tsx
@@ -1,6 +1,6 @@
 import "../styles/bracketDisplay.css";
 import React from "react";
-import Round1 from "../utilities/Round";
+import Round from "../utilities/Round";
 import Championship from "../utilities/Championship";
 
 const Select4 = () => {
@@ -8,7 +8,7 @@ const Select4 = () => {
         <>
             <div className="bracket-left">
                 <div className="round">
-                    <Round1 direction="left" team1="Team 1" team2="Team 2"></Round1>
+                    <Round direction="left" team1="Team 1" team2="Team 2"></Round>
                 </div>
 
                 <Championship team1="Team 1"></Championship>
@@ -20,7 +20,7 @@ const Select4 = () => {
                 <Championship team1="Team 4"></Championship>
 
                 <div className="round">
-                    <Round1 direction="right" team1="Team 3" team2="Team 4"></Round1>
+                    <Round direction="right" team1="Team 3" team2="Team 4"></Round>
                 </div>
             </div>
         </>
