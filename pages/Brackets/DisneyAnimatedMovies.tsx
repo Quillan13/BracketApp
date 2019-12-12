878a5435a299af22e0ceaade519abf3e41be3cb0
Quillan Ogle <quillan.ogle@ruralsource.com>
2019-12-12 13:55:22 -0500

    fix compile and build

---
+++ pages/Brackets/DisneyAnimatedMovies.tsx
@@ -0,0 +1,16 @@
+// This is dummy code to get app to compile
+
+import React from "react";
+import { Button } from "@material-ui/core";
+
+const DisneyAnimatedMovies = () => {
+    return (
+        <>
+            <Button href="/Bracket">Build a Bracket</Button>
+            <br />
+            {/* <Button href="Brackets/DisneyAnimatedMovies">Disney Animated Movies</Button> */}
+        </>
+    );
+};
+
+export default DisneyAnimatedMovies;
