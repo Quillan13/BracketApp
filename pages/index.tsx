d9dbbd099080faab34c2b57f378481f6b638f5f2
Quillan Ogle <quillan.ogle@ruralsource.com>
2019-12-12 10:30:25 -0500

    refactor select 32

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { Button } from '@material-ui/core';
+import React from "react";
+import { Button } from "@material-ui/core";
 
 const Home = () => {
-	return (
-		<>
-			<Button href="/Bracket">Build a Bracket</Button>
-			<br />
-			<Button href="/DisneyAnimatedMovies">Disney Animated Movies</Button>
-		</>
-	);
+    return (
+        <>
+            <Button href="/Bracket">Build a Bracket</Button>
+            <br />
+            {/* <Button href="Brackets/DisneyAnimatedMovies">Disney Animated Movies</Button> */}
+        </>
+    );
 };
 
 export default Home;
